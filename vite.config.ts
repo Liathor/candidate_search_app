15525957acfee176f319825603fe3ff4da6e5090
Liathor <dyermisha@gmail.com>
2025-03-19 18:50:50 -0400

    added allowedHosts to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,7 @@ export default defineConfig({
     host: '0.0.0.0', // Bind to 0.0.0.0 to listen on all interfaces (needed for Render)
     port: Number(process.env.PORT) || 3000, // Use the PORT provided by Render, fallback to 3000
   },
+  preview: {
+    allowedHosts: ['candidate-search-app-rc2q.onrender.com'], // Add this host to allowed hosts
+  },
 });
